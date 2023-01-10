9c258a56d5a03165da2e63286cc337be06814c31
Kai Thomas <45699223+kaiThomas236@users.noreply.github.com>
2023-01-10 16:10:00 -0700

    so much new stuff...

--- app.js
+++ app.js
@@ -13,8 +13,10 @@ app.set('view engine', 'ejs');
 mongoose.connect("mongodb+srv://admin-kai:test123@cluster0.dyrs9ij.mongodb.net/fishdexDB");
 
 const fishSchema = new mongoose.Schema({
+    _id: Number,
     commonName: String,
     scientificName: String,
+    imagePath: String,
     pH: String, 
     temp: String,
     tankSize: String
@@ -22,17 +24,93 @@ const fishSchema = new mongoose.Schema({
 
 const Fish = mongoose.model("Fish", fishSchema);
 
-app.get("/", function (req, res) {
-    const fish = new Fish({
-        commonName: "Betta",
-        scientificName: "Betta Splendens",
-        pH: "6.8 - 7.8",
-        temp: "75degF - 82degF",
-        tankSize: "5gal"
-    });
-    fish.save();
-    res.render('home');
+const fish1 = new Fish({
+    _id: 1,
+    commonName: "Betta",
+    scientificName: "Betta Splendens",
+    imagePath: "images/black-square.png",
+    pH: "6.8 - 7.8",
+    temp: "75degF - 82degF",
+    tankSize: "5gal"
+});
+const fish2 = new Fish({
+    _id: 2,
+    commonName: "Betta",
+    scientificName: "Betta Splendens",
+    imagePath: "images/black-square.png",
+    pH: "6.8 - 7.8",
+    temp: "75degF - 82degF",
+    tankSize: "5gal"
+});
+const fish3 = new Fish({
+    _id: 3,
+    commonName: "Betta",
+    scientificName: "Betta Splendens",
+    imagePath: "images/black-square.png",
+    pH: "6.8 - 7.8",
+    temp: "75degF - 82degF",
+    tankSize: "5gal"
+});
+const fish4 = new Fish({
+    _id: 4,
+    commonName: "Betta",
+    scientificName: "Betta Splendens",
+    imagePath: "images/black-square.png",
+    pH: "6.8 - 7.8",
+    temp: "75degF - 82degF",
+    tankSize: "5gal"
+});
+const fish5 = new Fish({
+    _id: 5,
+    commonName: "Betta",
+    scientificName: "Betta Splendens",
+    imagePath: "images/black-square.png",
+    pH: "6.8 - 7.8",
+    temp: "75degF - 82degF",
+    tankSize: "5gal"
+});
+const fish6 = new Fish({
+    _id: 6,
+    commonName: "Betta",
+    scientificName: "Betta Splendens",
+    imagePath: "images/black-square.png",
+    pH: "6.8 - 7.8",
+    temp: "75degF - 82degF",
+    tankSize: "5gal"
+});
 
+const defaultFish = [fish1, fish2, fish3, fish4, fish5, fish6];
+
+app.get("/", function (req, res) {
+    
+    Fish.find({}, function (err, fishList) {
+        if (fishList.length === 0) {
+            Fish.insertMany(defaultFish, function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Saved all the default fish.");
+                };
+            });
+            res.redirect("/");
+        } else {
+            res.render('home', { fishes: fishList });
+        };
+    });    
+
+});
+
+app.get("/fish/:fishID", function (req, res) {
+    
+    Fish.find({ _id: req.params.fishID}, function(err, fishList) {
+        if (err) {
+            console.log(err);
+        } else {
+            var fish = fishList[0];
+            res.render('fishdetails', {fish});
+        };
+        
+    });
 });
 
 app.get("/fish", function(req, res) {
@@ -45,9 +123,16 @@ app.listen(3000, function () {
 });
 
 /* TODO:  
-change from 6 cards to infinite cards with for loop
 figure out how to bulk upload fish
+finish variables on fishdetails
+work out display fish name over image on home page
+search function? keywords? what about typos?
+you might also like...? a similarity algorithm? could be random to start*/
+
+/* DONE:
+show database info on fish details page
 flesh out fish details page
 add ID to db manually so it is not so messy
+change from 6 cards to infinite cards with for loop
 use ID to call fish details page
-search function? */
+ */
